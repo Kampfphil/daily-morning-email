54ebd38ce1f1fd44dc19b0a2edb4f644ae5aee15
Kampfphil <77541293+Kampfphil@users.noreply.github.com>
2021-01-16 18:09:46 +0100

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,14 @@ from daily_Message import daily_Message
 import schedule
 import time
 
-schedule.every().day.at("17:50").do(daily_Message.send_email)
+from datetime import datetime
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
+
+schedule.every().day.at("18:30").do(daily_Message.send_email)
 
 while True:
     print("Running")
